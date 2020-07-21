af8f128f33a5fa8b7a105f256b8f26a6d30afc85
김준호 <enddl0121@gmail.com>
2020-07-21 17:32:02 +0900

    remove랑 수정이랑, 메모랑, 서버연결 해야함

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import _ from 'lodash'
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss'
 
-
+const DragAndDropCalendar = withDragAndDrop(Calendar)
 
 class App extends React.Component {
   constructor(...args) {
     super(...args)
-
     this.state = {
       events: _.cloneDeep(events),
       dayLayoutAlgorithm: 'no-overlap',
+      displayDragItemInCell: true,
     }
+    this.moveEvent = this.moveEvent.bind(this)
+    this.newEvent = this.handleSelect.bind(this)
   }
 
   handleSelect = ({ start, end }) => {
@@ -34,14 +37,80 @@ class App extends React.Component {
       })
   }
 
+
+  handleDragStart = event => {
+    this.setState({ draggedEvent: event })
+  }
+
+  dragFromOutsideItem = () => {
+    return this.state.draggedEvent
+  }
+
+  onDropFromOutside = ({ start, end, allDay }) => {
+    const { draggedEvent } = this.state
+
+    const event = {
+      id: draggedEvent.id,
+      title: draggedEvent.title,
+      start,
+      end,
+      allDay: allDay,
+    }
+
+    this.setState({ draggedEvent: null })
+    this.moveEvent({ event, start, end })
+  }
+
+  moveEvent = ({ event, start, end, isAllDay: droppedOnAllDaySlot }) => {
+    const { events } = this.state
+
+    let allDay = event.allDay
+
+    if (!event.allDay && droppedOnAllDaySlot) {
+      allDay = true
+    } else if (event.allDay && !droppedOnAllDaySlot) {
+      allDay = false
+    }
+
+    const nextEvents = events.map(existingEvent => {
+      return existingEvent.title === event.title
+        ? { ...existingEvent, start, end }
+        : existingEvent
+    })
+
+    this.setState({
+      events: nextEvents,
+    })
+
+  }
+
+  resizeEvent = ({ event, start, end }) => {
+    const { events } = this.state
+
+    const nextEvents = events.map(existingEvent => {
+      console.log(existingEvent);
+      console.log(event.title);
+
+      return existingEvent.title === event.title
+        ? { ...existingEvent, start, end }
+        : existingEvent
+    })
+
+    this.setState({
+      events: nextEvents,
+    })
+  }
+
+
+
   render() {
     const localizer = momentLocalizer(moment)
     return (
       <>
         <h1>일정관리</h1>
-        <Calendar
+        <DragAndDropCalendar
           style={{ height: 800 ,width: '100%' }}
-          popup
+          popup={true}
           selectable
           localizer={localizer}
           events={this.state.events}
@@ -51,6 +120,16 @@ class App extends React.Component {
           onSelectEvent={event => alert(event.title)}
           onSelectSlot={this.handleSelect}
           dayLayoutAlgorithm={this.state.dayLayoutAlgorithm}
+
+
+          onEventDrop={this.moveEvent}
+          onEventResize={this.resizeEvent}
+          onDragStart={console.log}
+          dragFromOutsideItem={
+          this.state.displayDragItemInCell ? this.dragFromOutsideItem : null
+        }
+        onDropFromOutside={this.onDropFromOutside}
+        handleDragStart={this.handleDragStart}
         />
       </>
     )
